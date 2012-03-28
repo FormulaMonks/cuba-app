dc2770ad11979c9cbebbd4d5d0807defe767676d
Cyril David <cyx.ucron@gmail.com>
2012-03-29 05:56:33 +0800

    Require malone in shotgun.

--- shotgun.rb
+++ shotgun.rb
@@ -2,6 +2,7 @@ $:.unshift(*Dir[File.expand_path("vendor/*/lib", File.dirname(__FILE__))])
 
 require "cuba"
 require "cuba/contrib"
+require "malone"
 require "mote"
 require "ohm"
 require "pbkdf2"
