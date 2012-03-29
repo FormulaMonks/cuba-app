e13d1ce8a2102dd455c9cbce6a5313318dc365c6
Cyril David <cyx.ucron@gmail.com>
2012-03-29 18:16:58 +0800

    Use port 9393 instead (optimize for shotgun).
    Also default redis to DB 0.

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-APP_HOST=localhost:9292
+APP_HOST=localhost:9393
 MAIL_FROM=no-reply@sitename.com
-REDIS_URL=redis://127.0.0.1:6379/15
+REDIS_URL=redis://127.0.0.1:6379/0
 MALONE_URL=smtp://user:pass@smtp.gmail.com:587
