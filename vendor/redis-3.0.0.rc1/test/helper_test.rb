ccc540a105c437ebfe17a27a39df7ae16e15b9e3
Cyril David <cyx.ucron@gmail.com>
2012-04-24 14:29:52 +0800

    Remove vendored libs in favor of adding them to .gems.

--- vendor/redis-3.0.0.rc1/test/helper_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path("./helper", File.dirname(__FILE__))
-
-test "version_str_to_i" do
-  assert_equal 200000, version_str_to_i('2.0.0')
-  assert_equal 202020, version_str_to_i('2.2.2')
-  assert_equal 202022, version_str_to_i('2.2.22')
-  assert_equal 222222, version_str_to_i('22.22.22')
-end
